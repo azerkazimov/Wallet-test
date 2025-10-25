f4e371b38c5baba49c94955ce78da55bfd35fedc
Azer Kazimov <azer.kazimov@yahoo.com>
2025-10-25 13:48:34 +0400

    init

--- desktop-wallet/src/screens/HomeScreen.jsx
+++ desktop-wallet/src/screens/HomeScreen.jsx
@@ -8,6 +8,7 @@ export default function HomeScreen() {
         <div className="screen">
             <h1>Type here your code ...</h1>
             <p>You can type your code here...</p>
+            <p>You can type your code here...</p>
         </div>
     );
 }
